103e995c85d04f4f2d376ae0264a2f96794aa4b0
Ido Calman <ido@storemaven.com>
2017-08-20 13:41:23 +0300

    Setting state evaluation changed to switch-case

--- project/GameAux.c
+++ project/GameAux.c
@@ -21,9 +21,9 @@
 #define PRINT_EXIT_MESSAGE "Exiting...\n"
 
 // Game state messages
+#define color(x) ((x == 0) ? "black" : "white")
 #define NEXT_MOVE_MESSAGE "player - enter your move:\n"
 
-#define color(x) ((x == 0) ? "black " : "white ")
 
 void performMove(CHESS_GAME *game, GAME_MOVE *move) {
     MATRIX *board = game->gameBoard;
@@ -32,7 +32,6 @@ void performMove(CHESS_GAME *game, GAME_MOVE *move) {
     matSet(board, move->destRowIndex, move->destColIndex, piece); // set piece on current cell
 }
 
-
 int evaluateSettingStateCommand(CHESS_MATCH **matchPtr, SETTING_STATE_COMMAND *cmd) {
     SETTING_STATE_COMMAND_NAME name = cmd->command_name;
     int arg = cmd->commandArgument;
@@ -40,86 +39,88 @@ int evaluateSettingStateCommand(CHESS_MATCH **matchPtr, SETTING_STATE_COMMAND *c
 
     CHESS_MATCH *match = *matchPtr;
 
-    if (name == INVALID_COMMAND) {
-        printf(PRINT_INVALID_COMMAND_ERROR);
-        return 0;
-    }
-
-    if (name == GAME_MODE) {
-        if (arg == 1 || arg == 2) {
-            match->gameMode = arg;
-            (arg == 1) ? printf(PRINT_SET_ONE_PLAYER_COMMAND) : printf(PRINT_SET_TWO_PLAYERS_COMMAND);
-        } else {
-            printf(PRINT_WRONG_GAME_MODE_COMMAND);
-        }
-        return 0;
-    }
-
-    if (name == DIFFICULTY) {
-        if (match->gameMode == 1) {
-            if (arg >= 1 && arg <= 5) {
-                printf(PRINT_SET_NEW_DIFFICULTY, arg);
-                match->level = arg;
-            } else {
-                printf(PRINT_WRONG_DIFFICULTY_LEVEL_ERROR);
-            }
-        } else {
-            printf(PRINT_DIFFICULTY_LEVEL_WRONG_GAME_MODE);
-        }
-        return 0;
-    }
-
-    if (name == USER_COLOR) {
-        if (match->gameMode == 1){
-            if (arg == 0){
-                printf(PRINT_SET_NEW_USER_COLOR, "black");
-                match->userColor = arg;
-            } else if (arg == 1){
-                printf(PRINT_SET_NEW_USER_COLOR, "white");
-                match->userColor = arg;
-            } else {
-                printf(PRINT_WRONG_USER_COLOR_ERROR);
-            }
-        } else {
-            printf(PRINT_USER_COLOR_WRONG_GAME_MODE);
-        }
-        return 0;
-    }
-
-    if (name == LOAD){
-        CHESS_MATCH *newMatch = parseXMLGameFile(addressArg);
-        if (newMatch == NULL){
-            printf(PRINT_LOAD_FILE_ERROR);
+    switch(name) {
+        case INVALID_COMMAND : {
+            printf(PRINT_INVALID_COMMAND_ERROR);
             return 0;
         }
-        CHESS_MATCH *destroyMatch = match;
-        *matchPtr = newMatch;
-        destroyChessMatch(destroyMatch);
-        return 1;
-    }
 
-    if (name == DEFAULT){
-        printf(PRINT_GAME_SETTING_DEFAULT);
-        resetMatchSettings(match);
-        return 0;
-    }
+        case GAME_MODE : {
+            if (arg == 1 || arg == 2) {
+                match->gameMode = arg;
+                (arg == 1) ? printf(PRINT_SET_ONE_PLAYER_COMMAND) : printf(PRINT_SET_TWO_PLAYERS_COMMAND);
+            } else {
+                printf(PRINT_WRONG_GAME_MODE_COMMAND);
+            }
+            return 0;
+        }
 
-    if (name == PRINT_SETTING){
-        printMatchSettings(match);
-        return 0;
-    }
+        case DIFFICULTY : {
+            if (match->gameMode == 1) {
+                if (arg >= 1 && arg <= 5) {
+                    printf(PRINT_SET_NEW_DIFFICULTY, arg);
+                    match->level = arg;
+                } else {
+                    printf(PRINT_WRONG_DIFFICULTY_LEVEL_ERROR);
+                }
+            } else {
+                printf(PRINT_DIFFICULTY_LEVEL_WRONG_GAME_MODE);
+            }
+            return 0;
+        }
 
-    if (name == QUIT){
-        printf(PRINT_EXIT_MESSAGE);
-        return 2;
-    }
+        case USER_COLOR : {
+            if (match->gameMode == 1){
+                if (arg == 0){
+                    printf(PRINT_SET_NEW_USER_COLOR, "black");
+                    match->userColor = arg;
+                } else if (arg == 1){
+                    printf(PRINT_SET_NEW_USER_COLOR, "white");
+                    match->userColor = arg;
+                } else {
+                    printf(PRINT_WRONG_USER_COLOR_ERROR);
+                }
+            } else {
+                printf(PRINT_USER_COLOR_WRONG_GAME_MODE);
+            }
+            return 0;
+        }
 
-    if (name == START){
-        initGameBoard(match->game);
-        return 1;
-    }
+        case LOAD : {
+            CHESS_MATCH *newMatch = parseXMLGameFile(addressArg);
+            if (newMatch == NULL){
+                printf(PRINT_LOAD_FILE_ERROR);
+                return 0;
+            }
+            CHESS_MATCH *destroyMatch = match;
+            *matchPtr = newMatch;
+            destroyChessMatch(destroyMatch);
+            return 1;
+        }
 
-    return -1; // this should not happen. IF it does - there is a bug in the command parsing module
+        case DEFAULT : {
+            printf(PRINT_GAME_SETTING_DEFAULT);
+            resetMatchSettings(match);
+            return 0;
+        }
+
+        case PRINT_SETTING : {
+            printMatchSettings(match);
+            return 0;
+        }
+
+        case QUIT : {
+            printf(PRINT_EXIT_MESSAGE);
+            return 2;
+        }
+
+        case START : {
+            initGameBoard(match->game);
+            return 1;
+        }
+
+        default : return -1; // this should not happen. IF it does - there is a bug in the command parsing module
+    }
 }
 
 bool initiateChessMatch(CHESS_MATCH **match){
@@ -133,6 +134,10 @@ bool initiateChessMatch(CHESS_MATCH **match){
     return retValue;
 }
 
+int evaluateGameStateCommand(CHESS_GAME *game, SETTING_STATE_COMMAND *cmd, int mode) {
+
+}
+
 void initiateChessGame(CHESS_MATCH *match) {
     CHESS_GAME *game = match->game;
     int mode = match->gameMode;
@@ -140,8 +145,8 @@ void initiateChessGame(CHESS_MATCH *match) {
 
     while (status == 0) {
         printChessGameBoard(game);
-        printf(color(game->currentPlayer));
-        printf(NEXT_MOVE_MESSAGE);
-        status = 1;
+        printf("%s %s", color(game->currentPlayer), NEXT_MOVE_MESSAGE);
+        SETTING_STATE_COMMAND *cmd = parseUserCommand();
+        status = evaluateGameStateCommand(game, cmd, mode);
     }
 }
