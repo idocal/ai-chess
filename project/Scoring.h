e92b03d172599b7677a7552c970f4817eb5271cb
Ido Calman <ido@storemaven.com>
2017-08-25 12:04:46 +0300

    Naïve scoring function

---
+++ project/Scoring.h
@@ -0,0 +1,12 @@
+//
+// Created by Ido on 8/25/17.
+//
+
+#ifndef PROJECT_SCORING_H
+#define PROJECT_SCORING_H
+
+#include "GameRepresenation.h"
+
+int score(CHESS_GAME *game);
+
+#endif //PROJECT_SCORING_H
