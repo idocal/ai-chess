8c25d5e79c6506c06c4c8f6eeaf540c77d2858fe
ofri64 <ofri64@gmail.com>
2017-09-19 09:31:43 -0700

    changed the scoring function in expert level. removed the all possible moves check because it is too much runtime consuming

--- project/Scoring.c
+++ project/Scoring.c
@@ -55,34 +55,6 @@ int expertPieceLegend(char piece) {
     return 0; // have to add this in order to compile the code under Werror flag
 }
 
-int threatenedScore(CHESS_GAME *game, char player) {
-    MATRIX *board = game->gameBoard;
-    int score = 0;
-    for (int x = 0; x < nRows; x++) {
-        for (int y = 0; y < nCols; y++) {
-            char piece = matGet(board, x, y);
-            if (pieceOwner(piece, player) == 1 && piece != 'm' && piece != 'M') { // not checking pawns
-                int pieceThreatenedCounter = 0;
-                for (int i = 0; i < nRows; i++) {
-                    for (int j = 0; j < nCols; j++) {
-                        char opponentPiece = matGet(board, i, j);
-                        char opponent = opponent(player);
-                        if (pieceOwner(opponentPiece, opponent) == 1) { // check opponent piece possible moves
-                            MATRIX *possibleMoves = piecePossibleMoves(game, opponentPiece, i, j, true);
-                            pieceThreatenedCounter += matGet(possibleMoves, x, y); // if possible move is (x,y) add to counter
-                            matDestroy(possibleMoves);
-                        }
-                    }
-                }
-
-                score += expertPieceLegend(piece) * pieceThreatenedCounter;
-
-            }
-
-        }
-    }
-    return score;
-}
 
 int score(CHESS_GAME *game) {
     MATRIX *board = game->gameBoard;
@@ -95,36 +67,7 @@ int score(CHESS_GAME *game) {
     return score;
 }
 
-int countPossibleMoves(CHESS_GAME *game) {
-    int count = 0;
 
-    for (int x = 0; x < nRows; x++) {
-        for (int y = 0; y < nCols; y++) {
-            char piece = matGet(game->gameBoard, x, y);
-            if (piece >= 'a' && piece <= 'z') { // piece is white
-                MATRIX *possibleMoves = piecePossibleMoves(game, piece, x, y, true);
-                for (int i = 0; i < nRows; i++) {
-                    for (int j = 0; j < nRows; j++) {
-                        count += (int) (matGet(possibleMoves, i, j));
-                    }
-                }
-                matDestroy(possibleMoves);
-            }
-            else if (piece >= 'A' && piece <= 'Z') { // piece is black
-                MATRIX *possibleMoves = piecePossibleMoves(game, piece, x, y, true);
-                for (int i = 0; i < nRows; i++) {
-                    for (int j = 0; j < nRows; j++) {
-                        count -= (int) (matGet(possibleMoves, i, j));
-                    }
-                }
-                matDestroy(possibleMoves);
-            }
-
-        }
-    }
-
-    return count;
-}
 
 int pawnStructure(CHESS_GAME *game) {
     int defendedPawns = 0;
@@ -165,10 +108,8 @@ int expertScore(CHESS_GAME *game) {
     int expertModeScore = 0;
 
     // combine naive scoring function with expert scoring function. give a different weight to each scoring function
-    expertModeScore += 100 * score(game); // original score function gets weight 100
-    expertModeScore += 50 * countPossibleMoves(game); // it's important to have a more possible moves than the opponent, weight is 50
-    expertModeScore += 10 * isCheckScore(game); // The current player is switched before the move, so increment if the player is checked
+    expertModeScore += 50 * score(game); // original score function gets weight 50
+    expertModeScore += 25 * isCheckScore(game); // The current player is switched before the move, so increment if the player is checked
     expertModeScore += 5 * pawnStructure(game); // defended pawns are a goal when all other moves lead to the same score
-
     return expertModeScore;
 }
